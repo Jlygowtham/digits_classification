61ebadca28765e07858734b93ae2ede7421e41c4
Gowtham E <91894759+Jlygowtham@users.noreply.github.com>
2026-02-18 21:14:20 +0530

    Refactor prediction handling and update model path

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from typing import List
 import numpy as np
 import joblib
 
 app = FastAPI()
 
-model = joblib.load('digits_model.joblib')
+model = joblib.load('./digits_model.joblib')
 
 class Features(BaseModel):
-    features : list
+    features : List[List[float]]
 
 
 @app.get('/')
@@ -19,9 +20,9 @@ def welcome():
 @app.post('/predict')
 def predict(data: Features):
     try:
-        x = np.array(data.features).reshape(1,-1)
-        predict_value = model.predict(x)[0]
-        return {'predict_value': int(predict_value), 'status': 200}
+        x = np.array(data.features)
+        predict_value = model.predict(x)
+        return {'predict_value': predict_value.tolist(), 'status': 200}
     except Exception as e:
         print('Exception: ', str(e))
         return HTTPException(status_code=500, detail=str(e))
